d739e38e3ac3ef1ba1811bd551529326fb64655b
wert310 <310wert@gmail.com>
2025-07-23 19:58:15 +0200

    rename

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
-  name = "webcam";
+  name = "camera";
   src = ./.;
   buildInputs = [ gcc sdl3 ];
   buildPhase = ''
     mkdir -p $out/bin
-    $CC webcam.c -o $out/bin/webcam -lSDL3
+    $CC camera.c -o $out/bin/camera -lSDL3
   '';
 }
