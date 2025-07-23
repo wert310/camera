d47c8b6f0dee118b25994617e695953bed9223ef
wert310 <310wert@gmail.com>
2025-07-23 19:47:52 +0200

    fix fullscreen, add readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# webcam
+# camera
+
+Display the live feed from a camera device.
+
+This is essentially the code of [this SDL example](https://github.com/libsdl-org/SDL/blob/main/examples/camera/01-read-and-draw/read-and-draw.c), with command line arguments to select the camera, the window size and a keyboard shortcut to enable and disable full screen mode.
--- webcam.c
+++ webcam.c
@@ -2,18 +2,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-
-// https://github.com/libsdl-org/SDL/blob/main/examples/camera/01-read-and-draw/read-and-draw.c
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_Camera *camera = NULL;
 static SDL_Texture *texture = NULL;
 static int height = 0, width = 0;
-
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   goto start;
@@ -105,12 +102,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
   }
   if (event->type == SDL_EVENT_KEY_DOWN) {
     if (event->key.key == SDLK_F) {
-      if (SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN) {
-        SDL_SetWindowFullscreen(window, false);
-        SDL_SetWindowSize(window, width, height);
-      } else {
-        SDL_SetWindowFullscreen(window, true);
-      }
+      SDL_SetWindowFullscreen(window, !(SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN));
     }
   }
   return SDL_APP_CONTINUE;
